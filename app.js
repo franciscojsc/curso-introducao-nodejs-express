85e4b8e800d8e8de30bb4bb4b80f2ac6ebb0ff4a
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-10 12:05:40 -0300

    Server básico que retorna HTML como resposta de acordo com a url

--- app.js
+++ app.js
@@ -3,7 +3,39 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
     var pagina = req.url;
     console.log(pagina);
-    res.end('Url = '+ pagina);
+
+    if(pagina == '/contato'){
+        res.end(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>
+                    Introdução ao Node JS
+                </title>
+            </head>
+            <body>
+                <h1>Página de Contato</h1>
+            </body>
+        </html>
+        `);
+    } else {
+        res.end(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>
+                    Introdução ao Node JS
+                </title>
+            </head>
+            <body>
+                <h1>Página de Home</h1>
+            </body>
+        </html>
+        `);
+    }
+    
 });
 
 console.log('http://localhost:3000')
