66c3025b47b17c968bc96b33d1730fbca454bf9d
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-12 14:45:27 -0300

    Realizando a conexão com o banco de dados MYSQL

--- app/models/clienteModel.js
+++ app/models/clienteModel.js
@@ -1,6 +1,20 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'curso_node'
+});
+
 module.exports = function() {
 
     this.all = function() {
+
+        con.query('select * from clientes', function(error, resultado) {
+            console.log(resultado);
+        });
+
         return [{nome: "Francisco", email: "contato@franciscochaves.com.br"}];
     };
 
