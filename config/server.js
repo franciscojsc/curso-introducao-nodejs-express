c84f57b98dd2d34edf1454fa3c55252544ed3963
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-13 13:09:06 -0300

    Adicionando configurações do body-parser

--- config/server.js
+++ config/server.js
@@ -1,11 +1,15 @@
 module.exports = function() {
 
     var express = require('express');
+    var bodyParser = require('body-parser');
 
     var app = express();
     
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
     
     var rotas = require('../app/routes/web');
     rotas(app);
