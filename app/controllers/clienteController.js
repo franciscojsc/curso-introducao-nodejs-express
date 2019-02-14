d779324359a78f112d7825a93d2cdb534872776e
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-14 00:43:05 -0300

    Adicionando validações nos dados do formulário no backend

--- app/controllers/clienteController.js
+++ app/controllers/clienteController.js
@@ -2,13 +2,28 @@ var clienteModels = require('../models/clienteModel')();
 
 module.exports.index = function(req, res) {
     clienteModels.all(function(error, resultado) {
-        res.render('site/index', {clientes: resultado});
+        res.render('site/index', {clientes: resultado, erros: {}, dados: {}});
     });
 };
 
 module.exports.store = function(req, res) {
 
     var dados = req.body;
+
+    req.assert('nome', 'Preencha o nome').notEmpty();
+    req.assert('email', 'Preencha o email').notEmpty();
+    req.assert('nome', 'Nome deve ter de 3 a 20 caracteres').len(3, 20);
+    req.assert('email', 'Preencha um E-mail válido').isEmail();
+
+    var validacaoErros = req.validationErrors();
+
+    if(validacaoErros){
+        console.log(validacaoErros);
+        clienteModels.all(function(erro, resultado){
+            res.render('site/index', {clientes: resultado, erros: validacaoErros, dados: dados});
+        });
+        return;
+    }
 
     clienteModels.save(dados, function(error, resultado) {
         try{
